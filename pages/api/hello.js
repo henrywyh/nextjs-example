82aca0243e5efe735e26747ed027c6fb42cedcdd
henrywyh <yanhowong20@hotmail.com>
2022-08-02 11:55:09 +0800

    init

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,6 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+  // do backend stuff , like calling databse, calling API
+  res.status(200).json({ name: 'Backend Response' })
 }
